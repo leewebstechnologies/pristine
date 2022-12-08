69b948ff0900f115322f47bb5287833ebfe539ba
leewebstechnologies <aliuajibade317@gmail.com>
2022-12-07 22:47:22 -0800

    top class created as well as topLeft, topCenter and topRight

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,11 @@
+import "./topbar.css";
+
+const TopBar = () => {
+  return <div className="top">
+    <div className="topLeft">l</div>
+    <div className="topCenter">c</div>
+    <div className="topRight">r</div>
+  </div>;
+};
+
+export default TopBar;
