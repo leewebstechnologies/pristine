3e65876594918880f035fd047adb72ae7d80642d
leewebstechnologies <aliuajibade317@gmail.com>
2022-12-09 08:10:46 -0800

    topbar completed

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -1,11 +1,33 @@
 import "./topbar.css";
 
 const TopBar = () => {
-  return <div className="top">
-    <div className="topLeft">l</div>
-    <div className="topCenter">c</div>
-    <div className="topRight">r</div>
-  </div>;
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fa-brands fa-square-facebook"></i>
+        <i className="topIcon fa-brands fa-square-twitter"></i>
+        <i className="topIcon  fa-brands fa-square-pinterest"></i>
+        <i className="topIcon fa-brands fa-square-instagram"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img
+          className="topImg"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Bilal_Philips.jpg/220px-Bilal_Philips.jpg"
+          alt=""
+        />
+        <i class="topSearchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
+  );
 };
 
 export default TopBar;
