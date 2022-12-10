250b4bd4d7b09853ba3f7735c2ecd99972801323
leewebstechnologies <aliuajibade317@gmail.com>
2022-12-09 22:01:55 -0800

    sidebar added

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className="header">
       <div className="headerTitles">
         <span className="headerTitleSm">React & Node</span>
-        <span className="headerTitleBg">Blog</span>
+        <span className="headerTitleLg">Blog</span>
       </div>
       <img
         className="headerImg"
